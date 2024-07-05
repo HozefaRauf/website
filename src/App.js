e44417c677212ed8d5163fd30cb07f7fa45f2790
hozefarauf <hozefarauf@gmail.com>
2024-07-05 05:45:28 +0000

    new commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
+import './style/components.css';
+import buttoon from'./components/button.js'
 
 function App() {
   return (
     <div>
       <h1>React App</h1>
       <h2>hh</h2>
+      <div>
+        <span>{buttoon}</span> 
+        </div>
     </div>
   );
 }
